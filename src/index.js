d0637b5440893da0f5e8cb5055aaf0f9b9e9eb22
rizamoyi <samwelmoyi@gmail.com>
2022-05-15 12:00:46 +0300

    redux logger

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import {
   legacy_createStore as createStore,
   applyMiddleware,
   combineReducers,
 } from 'redux';
-import { createLogger } from 'redux-logger';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
@@ -15,7 +15,6 @@ import 'tachyons';
 
 import { searchRobots, requestRobots } from './reducers';
 
-const logger = createLogger();
 const rootReducer = combineReducers({ searchRobots, requestRobots });
 
 const store = createStore(
